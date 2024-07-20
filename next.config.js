b1346d2d24068c4d7e6191ab0bc62fa9fe14c224
jeff.jianbo.zhu <jeff.jianbo.zhu@accenture.com>
2024-07-21 00:25:24 +0800

    feat(project)finished image crop and upload

--- next.config.js
+++ next.config.js
@@ -65,7 +65,7 @@ module.exports = () => {
     output: 'standalone',
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
     eslint: {
-      dirs: ['app', 'components', 'layouts', 'scripts', 'pages'],
+      dirs: ['app', 'components', 'layouts', 'scripts', 'pages','libs','services','utils'],
     },
     async rewrites() {
       return [
